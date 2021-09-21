23aa6b8259027ce76df51887cd94b2e4788ed7e0
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-09-21 19:30:02 -0400

    commit tree 889fd9d4a1b255eb1c2b2216f2ae0970f127baef

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+function superbowlWin(array) {
+    const result = array.find(function(record) { return record.result === "W"})
+    if (result) {
+        return result.year
+    } else {
+        return undefined
+    }
+}
